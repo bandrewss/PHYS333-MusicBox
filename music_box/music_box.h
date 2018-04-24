b1aa1fd1db6b3c0f9952ed4df8928b42beda1052
Ben Andrews <ben13100@msn.com>
2018-04-23 21:05:18 -0500

    Added octaves 1 and 2.

--- music_box/music_box.h
+++ music_box/music_box.h
@@ -6,38 +6,86 @@
 //  scaled to a pwm based on microseconds
 #define SCALER (1000000 /2 /
 
-#define A3 SCALER ( 220 ))
+#define C1  SCALER ( 33 ))     // Pedal C
+#define CS1 SCALER ( 35 ))
+#define DF1 CS1                
+#define D1  SCALER ( 37 ))
+#define DS1 SCALER ( 39 ))
+#define EF1 DS1
+#define E1  SCALER ( 41 ))
+#define F1  SCALER ( 44 ))
+#define FS1 SCALER ( 56 ))
+#define GF1 FS1
+#define G1  SCALER ( 49 ))
+#define GS1 SCALER ( 52 ))
+#define A1  SCALER ( 55 ))
+#define AS1 SCALER ( 58 ))
+#define BF1 AS1
+#define B1  SCALER ( 62 ))
+
+#define C2  SCALER ( 65 ))     // Deep C
+#define CS2 SCALER ( 69 ))
+#define DF2 CS2                 
+#define D2  SCALER ( 73 ))
+#define DS2 SCALER ( 77 ))
+#define EF2 DS2
+#define E2  SCALER ( 82 ))
+#define F2  SCALER ( 87 ))
+#define FS2 SCALER ( 93 ))
+#define GF2 FS2
+#define G2  SCALER ( 98 ))
+#define GS2 SCALER ( 103 ))
+#define A2  SCALER ( 110 ))
+#define AS2 SCALER ( 117 ))
+#define BF2 AS2
+#define B2  SCALER ( 123 ))
+
+#define C3  SCALER ( 131 ))     // C3
+#define CS3 SCALER ( 139 ))
+#define DF3 CS3                 
+#define D3  SCALER ( 147 ))
+#define DS3 SCALER ( 156 ))
+#define EF3 DS3
+#define E3  SCALER ( 165 ))
+#define F3  SCALER ( 175 ))
+#define FS3 SCALER ( 185 ))
+#define GF3 FS3
+#define G3  SCALER ( 195 ))
+#define GS3 SCALER ( 208 ))
+#define A3  SCALER ( 220 ))
 #define AS3 SCALER ( 233 ))
 #define BF3 AS3
-#define B3 SCALER ( 246 ))
-#define C4 SCALER ( 261 ))
+#define B3  SCALER ( 246 ))
+
+#define C4  SCALER ( 261 ))     // middle C
 #define CS4 SCALER ( 277 ))
-#define DF4 CS4
-#define D4 SCALER ( 293 ))
+#define DF4 CS4                 
+#define D4  SCALER ( 293 ))
 #define DS4 SCALER ( 311 ))
 #define EF4 DS4
-#define E4 SCALER ( 329 ))
-#define F4 SCALER ( 349 ))
+#define E4  SCALER ( 329 ))
+#define F4  SCALER ( 349 ))
 #define FS4 SCALER ( 370 ))
 #define GF4 FS4
-#define G4 SCALER ( 391 ))
+#define G4  SCALER ( 391 ))
 #define GS4 SCALER ( 415 ))
 #define AF4 GS4
-#define A4 SCALER ( 440 ))
+#define A4  SCALER ( 440 ))
 #define AS4 SCALER ( 466 ))
 #define BF4 AS4
-#define B4 SCALER ( 493 ))
-#define C5 SCALER ( 523 ))
+#define B4  SCALER ( 493 ))
+
+#define C5  SCALER ( 523 ))     // tenor C
 #define CS5 SCALER ( 554 ))
 #define DF5 CS5
-#define D5 SCALER ( 587 ))
+#define D5  SCALER ( 587 ))
 #define DS5 SCALER ( 622 ))
 #define EF5 DS5
-#define E5 SCALER ( 659 ))
-#define F5 SCALER ( 698 ))
+#define E5  SCALER ( 659 ))
+#define F5  SCALER ( 698 ))
 #define FS5 SCALER ( 739 ))
 #define GF5 FS5
-#define G5 SCALER ( 783 ))
+#define G5  SCALER ( 783 ))
 #define GS5 SCALER ( 830 ))
 #define AF5 GS5
 
